c3bf57e9c13eb0bcdea3d01588287b2955de6d21
pacheights <pearsonhbuck@gmail.com>
2021-01-24 16:05:56 -0800

    getState init and getZip updates

--- util.js
+++ util.js
@@ -1,7 +1,15 @@
 const getZip = (address) => {
-  const zipRegex = /\d{5}/g;
-  const zip = address.match(zipRegex);
-  return zip[zip.length - 1];
+  const zipRegex = /(\d{5}-?\d{4}|\d{5})/g;
+  const zipMatches = address.match(zipRegex);
+  const lastIndex = zipMatches.length - 1;
+  return zipMatches[lastIndex];
+};
+
+const getState = (address) => {
+  const stateRegex = /[A-Z]{2}/g;
+  const stateMatches = address.match(stateRegex);
+  const lastIndex = stateMatches.length - 1;
+  return stateMatches[lastIndex];
 };
 
 const getAddressParts = (address) => {
@@ -10,5 +18,6 @@ const getAddressParts = (address) => {
 
 module.exports = {
   getZip,
+  getState,
   getAddressParts,
 };
--- util.test.js
+++ util.test.js
@@ -1,7 +1,11 @@
-const { getZip } = require('./util');
+const { getZip, getState } = require('./util');
 
 const address = '1875 Turk St. Apt. 2, San Francisco, CA 94115';
 
 test('getting zip', () => {
   expect(getZip(address)).toBe('94115');
+});
+
+test('getting state', () => {
+  expect(getState(address)).toBe('CA');
 });
