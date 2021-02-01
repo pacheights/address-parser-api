7b5b354e4fc787ccdc2043738bd21b279e2a25d7
pacheights <pearsonhbuck@gmail.com>
2021-01-31 17:02:36 -0800

    getAptUnitSuite, getHouseNumber + tests

--- util.js
+++ util.js
@@ -12,12 +12,25 @@ const getState = (address) => {
   return stateMatches[lastIndex];
 };
 
+const getAptUnitSuite = (address) => {
+  const aptRegex = /\b(?=Apt|Suite|Unit)\w\S+\s\w+/gi;
+  const aptMatches = address.match(aptRegex);
+  return aptMatches[0];
+};
+
+const getHouseNumber = (address) => {
+  const houseNumberRegex = /^(\d+)/g;
+  const houseNumberMatches = address.match(houseNumberRegex);
+  return houseNumberMatches[0];
+};
+
 const getAddressParts = (address) => {
   const zip = getZip();
 };
 
 module.exports = {
+  getHouseNumber,
   getZip,
   getState,
-  getAddressParts,
+  getAptUnitSuite,
 };
--- util.test.js
+++ util.test.js
@@ -1,6 +1,10 @@
-const { getZip, getState } = require('./util');
+const { getHouseNumber, getZip, getState, getAptUnitSuite } = require('./util');
 
 const address = '1875 Turk St. Apt. 2, San Francisco, CA 94115';
+
+test('getting house number', () => {
+  expect(getHouseNumber(address)).toBe('1875');
+});
 
 test('getting zip', () => {
   expect(getZip(address)).toBe('94115');
@@ -8,4 +12,8 @@ test('getting zip', () => {
 
 test('getting state', () => {
   expect(getState(address)).toBe('CA');
+});
+
+test('getting apt, unit, or suite', () => {
+  expect(getAptUnitSuite(address)).toBe('Apt. 2');
 });
